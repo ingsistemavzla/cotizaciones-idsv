f0f590b1f74c6da9cc17fe1e4fb3dcdca29cad67
Ingenieros de Sistema Venezuela <ingenierosdesistemavenezuela@gmail.com>
2026-01-20 11:48:28 -0400

    Add two-column quote route and clean tests

--- src/App.tsx
+++ src/App.tsx
@@ -5,30 +5,33 @@ import { About } from './pages/About'
 import { InvoicePage } from './pages/Invoice'
 import { Landing } from './pages/Landing'
 import { QuotationPage } from './pages/Quotation'
+import { QuotationTwoColumnsPage } from './pages/QuotationTwoColumns'
 
 function App() {
   return (
-    <BrowserRouter basename="/cotizaciones-idsv">
-      <Routes>
-        <Route path="/" element={<Navigate to="/oferta-web-coaching" replace />} />
-        <Route path="/landing" element={<Landing />} />
-        <Route path="/invoice" element={<InvoicePage />} />
-        <Route path="/oferta-web-coaching" element={<QuotationPage />} />
-        <Route
-          path="/*"
-          element={
-            <Layout>
-              <Routes>
-                <Route path="/home" element={<Home />} />
-                <Route path="/about" element={<About />} />
-              </Routes>
-            </Layout>
-          }
-        />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter basename="/cotizaciones-idsv">
+        <Routes>
+          <Route path="/" element={<Navigate to="/oferta-web-icmf-acca" replace />} />
+          <Route path="/landing" element={<Landing />} />
+          <Route path="/invoice" element={<InvoicePage />} />
+          <Route path="/oferta-web-coaching" element={<QuotationPage />} />
+          <Route path="/oferta-web-icmf-acca" element={<QuotationTwoColumnsPage />} />
+          <Route
+            path="/*"
+            element={
+              <Layout>
+                <Routes>
+                  <Route path="/home" element={<Home />} />
+                  <Route path="/about" element={<About />} />
+                </Routes>
+              </Layout>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
 export default App
-
